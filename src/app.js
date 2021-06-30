4bbbc999cd8bc25e3f56fa6ad2b6d92e80f3cf9d
Roxanne <roxanne-m@users.noreply.github.com>
2021-06-30 14:45:22 -0700

    First commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+requires('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const app = express();
+
+const morganOption = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+
+module.exports = app;
