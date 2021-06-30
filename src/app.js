828b6c2326782d95a2d805f42418e367f6a3ff75
Roxanne <roxanne-m@users.noreply.github.com>
2021-06-30 14:59:13 -0700

    installed test libraries

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
-requires('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const helmet = require('helmet');
 
 const app = express();
@@ -10,5 +11,9 @@ const morganOption = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
 
 module.exports = app;
